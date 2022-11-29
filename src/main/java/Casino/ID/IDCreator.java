b05bc72b53a1082788f1ccfa6be68b34b32f6123
AlexanderLuz <saveli.gulas@gmail.com>
2022-11-29 10:51:36 +0100

    IDCreator added 2 AtomicIntegers to assign IDs to Tables and Players

--- src/main/java/Casino/ID/IDCreator.java
+++ src/main/java/Casino/ID/IDCreator.java
@@ -3,8 +3,12 @@ package Casino.ID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public final class IDCreator {
-    private static final AtomicInteger atomicInteger = new AtomicInteger(10000);
-    public static int getUniqueID() {
-        return atomicInteger.incrementAndGet();
+    private static final AtomicInteger atomicIntegerTable = new AtomicInteger(1000);
+    private static final AtomicInteger atomicIntegerPlayer = new AtomicInteger(10000);
+    public static int getUniqueTableID() {
+        return atomicIntegerTable.incrementAndGet();
+    }
+    public static int getUniquePlayerID() {
+        return atomicIntegerPlayer.incrementAndGet();
     }
 }
