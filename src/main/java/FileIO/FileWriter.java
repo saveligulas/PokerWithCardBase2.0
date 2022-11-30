2a058c060571a886cdf11784dadabf0afca2f9be
AlexanderLuz <saveli.gulas@gmail.com>
2022-11-30 10:33:29 +0100

    Added CSVWriter

--- src/main/java/FileIO/FileWriter.java
+++ src/main/java/FileIO/FileWriter.java
@@ -1,7 +1,27 @@
 package FileIO;
 
+import Table.Models.*;
+import com.opencsv.CSVWriter;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class FileWriter {
-    public void PlayerWriter(){
+    private final String CasinoDataSheetPath = "src/main/resources/CasinoDataSheet.csv";
+    private final String PlayerSheetPath = "src/main/resources/PlayerSheet.csv";
+    private final String TableSheetPath = "src/main/resources/TableSheet.csv";
+    public void PlayerWriter(Player player){
+        try{
+            CSVWriter writer = new CSVWriter(new FileWriter(PlayerSheetPath), CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER);
+            String[] values = new String[3];
+            values[0] = player.Name();
+            values[1] = String.valueOf(player.Stack());
+            values[2] = String.valueOf(player.ID());
+            writer.writeNext(values,false);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
     public void TableWriter(){
