b5caae4f9f19c246540a5511809d5feebc0269db
saveligulas <saveli.gulas@gmail.com>
2022-12-01 10:09:21 +0100

    Updated FileIO. Added Casino Record and tested FileReader.

--- src/main/java/Casino/Data/SyncData.java
+++ src/main/java/Casino/Data/SyncData.java
@@ -1,7 +1,12 @@
 package Casino.Data;
 
+import FileIO.FileReaderIO;
+import Table.Models.Player;
+
+import java.util.ArrayList;
+
 public class SyncData {
     public void FetchPlayerSheet() {
-
+        ArrayList<Player> PlayerArrayList = FileReaderIO.ReadAllPlayers();
     }
 }
