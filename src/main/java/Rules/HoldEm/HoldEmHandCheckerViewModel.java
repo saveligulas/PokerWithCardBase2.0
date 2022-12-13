70e7c679e5f0741f75973254c7ad405876d8066f
saveligulas <saveli.gulas@gmail.com>
2022-12-13 11:40:55 +0100

    Added rank and suit list and added method to sort them.

--- src/main/java/Rules/HoldEm/HoldEmHandCheckerViewModel.java
+++ src/main/java/Rules/HoldEm/HoldEmHandCheckerViewModel.java
@@ -1,6 +1,8 @@
 package Rules.HoldEm;
 
 import CardBase.Card;
+import CardBase.Rank;
+import CardBase.Suit;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -8,14 +10,23 @@ import java.util.Collections;
 public class HoldEmHandCheckerViewModel {
     private int value;
     private ArrayList<Card> cardList = new ArrayList<Card>();
+    private ArrayList<Rank> rankList = new ArrayList<>();
+    private ArrayList<Suit> suitList = new ArrayList<Suit>();
 
     public int checkAndGetHandValue(ArrayList<Card> cardArrayList) {
-        Collections.sort();
+
+
         return 0;
     }
 
-    public void getHand(ArrayList<Card> cardArrayList) {
+    public void getHandAndInitializeLists(ArrayList<Card> cardArrayList) {
         cardList = cardArrayList;
+        for(Card card:cardArrayList) {
+            rankList.add(card.getRank());
+            suitList.add(card.getSuit());
+        }
+        Collections.sort(rankList);
+        Collections.sort(suitList);
     }
 
     public void checkForPair() {
