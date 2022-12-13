690032595461cc795392286bbc6b058f4236593c
saveligulas <saveli.gulas@gmail.com>
2022-12-13 17:19:41 +0100

    Implemented straight checker and removed try catch block.

--- src/main/java/Rules/HoldEm/HoldEmHandCheckerViewModel.java
+++ src/main/java/Rules/HoldEm/HoldEmHandCheckerViewModel.java
@@ -8,12 +8,15 @@ import SuperClasses.Table;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class HoldEmHandCheckerViewModel {
     private int value;
     private ArrayList<Card> cardList = new ArrayList<Card>();
     private ArrayList<Rank> rankList = new ArrayList<>();
     private ArrayList<Suit> suitList = new ArrayList<Suit>();
+    private ArrayList<Rank> rankListWithoutDuplicates = new ArrayList<>();
 
     public HandStrengthModel checkAndGetHandValue(Player player, Table table) {
         getHandAndInitializeLists(player,table);
@@ -44,6 +47,11 @@ public class HoldEmHandCheckerViewModel {
             rankList.add(card.getRank());
             suitList.add(card.getSuit());
         }
+        Set<Rank> rankSetList = new HashSet<>(rankList);
+        rankListWithoutDuplicates.clear();
+        rankListWithoutDuplicates.addAll(rankSetList);
+        Collections.sort(rankListWithoutDuplicates);
+        Collections.reverse(rankListWithoutDuplicates);
         Collections.sort(rankList);
         Collections.reverse(rankList);
         System.out.println(rankList);
@@ -89,23 +97,27 @@ public class HoldEmHandCheckerViewModel {
         Rank placeholder = rankList.get(0);
         int counter = 0;
         for(int i = 0; i<rankList.size(); i++) {
-            if (rankList.get(i).getValue() == placeholder.getValue() && counter != 0 && rankList.get(i + 1).getValue() == placeholder.getValue() && counter != rankList.size()-1) {
-                return rankList.get(i).getValue();
+            if(counter != rankList.size()-1) {
+                if (rankList.get(i).getValue() == placeholder.getValue() && counter != 0 && rankList.get(i + 1).getValue() == placeholder.getValue()) {
+                    return rankList.get(i).getValue();
+                }
+                counter += 1;
+                placeholder = rankList.get(i);
             }
-            counter += 1;
-            placeholder = rankList.get(i);
         }
         return 0;
     }
 
     public int checkForStraight() {
-        for(int i = 0; i<(rankList.size()-4); i++) {
-            int value = rankList.get(i).getValue();
-            if(rankList.get(i+1).getValue() == value-1) {
-                if(rankList.get(i+2).getValue() == value-2) {
-                    if(rankList.get(i+3).getValue() == value-3) {
-                        if(rankList.get(i+4).getValue() == value-4) {
-                            return value;
+        if(rankListWithoutDuplicates.size()>=5) {
+            for (int i = 0; i < (rankListWithoutDuplicates.size() - 4); i++) {
+                int value = rankListWithoutDuplicates.get(i).getValue();
+                if (rankListWithoutDuplicates.get(i + 1).getValue() == value - 1) {
+                    if (rankListWithoutDuplicates.get(i + 2).getValue() == value - 2) {
+                        if (rankListWithoutDuplicates.get(i + 3).getValue() == value - 3) {
+                            if (rankListWithoutDuplicates.get(i + 4).getValue() == value - 4) {
+                                return value;
+                            }
                         }
                     }
                 }
