aeb94938679894f335857ae14b92ae5458cc79b9
saveligulas <saveli.gulas@gmail.com>
2022-12-13 15:57:25 +0100

    Reworked Ace in Rank.

--- src/main/java/CardBase/Rank.java
+++ src/main/java/CardBase/Rank.java
@@ -1,9 +1,9 @@
 package CardBase;
 
 public enum Rank {
-    ACE(1),TWO(2),THREE(3),FOUR(4),FIVE(5),
+    TWO(2),THREE(3),FOUR(4),FIVE(5),
     SIX(6),SEVEN(7),EIGHT(8),NINE(9),
-    TEN(10),JACK(11),QUEEN(12),KING(13);
+    TEN(10),JACK(11),QUEEN(12),KING(13),ACE(14);
 
     private final Integer cardValue;
     private Rank(Integer cardValue) {
