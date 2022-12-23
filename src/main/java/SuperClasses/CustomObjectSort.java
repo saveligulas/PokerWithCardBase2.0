6e0a317e37c68502f44fda06bf9ffec00264e67a
saveligulas <saveli.gulas@gmail.com>
2022-12-23 11:04:38 +0100

    Commented out CustomObjectSort.

--- src/main/java/SuperClasses/CustomObjectSort.java
+++ src/main/java/SuperClasses/CustomObjectSort.java
@@ -1,21 +1,18 @@
 package SuperClasses;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-
-public final class CustomObjectSort {
-    public static ArrayList<Player> sortByHandStrength(ArrayList<Player> playerList) {
-
-        playerList.sort(new Comparator<Player>() {
-
-            @Override
-            public int compare(Player player1, Player player2) {
-                Integer value1 = player1.HandStrength.Value();
-                Integer value2 = player2.HandStrength.Value();
-                return value1.compareTo(value2);
-            }
-
-        });
-        return playerList;
-    }
-}
+//public final class CustomObjectSort {
+//    public static ArrayList<Player> sortByHandStrength(ArrayList<Player> playerList) {
+//
+//        playerList.sort(new Comparator<Player>() {
+//
+//            @Override
+//            public int compare(Player player1, Player player2) {
+//                Integer value1 = player1.HandStrength.Value();
+//                Integer value2 = player2.HandStrength.Value();
+//                return value1.compareTo(value2);
+//            }
+//
+//        });
+//        return playerList;
+//    }
+//}
