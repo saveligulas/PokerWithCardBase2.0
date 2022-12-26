9c54adb39c45e8f82e69ac7437bfdf3123727519
saveligulas <saveli.gulas@gmail.com>
2022-12-26 22:34:44 +0100

    Added constructor to Stack.

--- src/main/java/SuperClasses/Stack.java
+++ src/main/java/SuperClasses/Stack.java
@@ -1,4 +1,13 @@
 package SuperClasses;
 
+import TablePlayerData.Models.StackModel;
+import TablePlayerData.ViewModels.StackViewModel;
+
 public class Stack {
+    StackModel Model;
+    StackViewModel ViewModel;
+    public Stack(int money) {
+        Model = new StackModel(money);
+        ViewModel = new StackViewModel();
+    }
 }
