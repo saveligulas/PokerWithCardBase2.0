7f72e492354beb62f5bc24c604ad943f3175e233
saveligulas <saveli.gulas@gmail.com>
2022-12-27 21:49:44 +0100

    Added standard Methods.

--- src/main/java/TablePlayerData/ViewModels/StackViewModel.java
+++ src/main/java/TablePlayerData/ViewModels/StackViewModel.java
@@ -1,4 +1,17 @@
 package TablePlayerData.ViewModels;
 
+import TablePlayerData.Models.StackModel;
+
 public class StackViewModel {
+    public void commitMoney(StackModel Model, int amount) {
+        Model.CommittedMoney += amount;
+    }
+
+    public void updateTotalWinnings() {
+
+    }
+
+    public void updateEnteredPot() {
+
+    }
 }
