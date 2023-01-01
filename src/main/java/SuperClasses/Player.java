3a4a187a42536576c09e77c5d840c6e248e6f3c3
saveligulas <saveli.gulas@gmail.com>
2023-01-01 20:08:03 +0100

    Removed print info Logic.

--- src/main/java/SuperClasses/Player.java
+++ src/main/java/SuperClasses/Player.java
@@ -37,10 +37,7 @@ public class Player {
     }
 
     public void printInfo() {
-        PrintMethods.printFiller(25,"-");
-        System.out.println();
-        System.out.println(ViewModel.getInfo(Model));
-        System.out.println(HandViewModel.getHand(HandModel));
+        ViewModel.printInfo(Model,HandViewModel,HandModel);
     }
 
     public void setHand(Card[] DealtHand) {
