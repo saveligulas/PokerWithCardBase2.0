c7b728ca186eac5e2a375e9fefa18af966035367
saveligulas <saveli.gulas@gmail.com>
2023-01-02 12:15:31 +0100

    Added Empty Suit.

--- src/main/java/CardBase/Suit.java
+++ src/main/java/CardBase/Suit.java
@@ -1,21 +1,13 @@
 package CardBase;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public enum Suit {
     HEARTS("Hearts", 1),SPADES("Spades", 2),DIAMONDS("Diamonds",3),CLUBS("Clubs",4), EMPTY_SUIT("",0);
     private final String suitName;
-    private AtomicInteger atomicInteger = new AtomicInteger(-1);
     public final int Index;
 
     private Suit(String suitName, int Index){
-        if(Index == 0) {
-            this.Index = atomicInteger.decrementAndGet();
-            this.suitName = "";
-        } else {
-            this.suitName = suitName;
-            this.Index = Index;
-        }
+        this.suitName = suitName;
+        this.Index = Index;
     }
 
     public String getSuitName() {
