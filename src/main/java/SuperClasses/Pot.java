950affc3fe1dd89e1a6c5849f68f1e5c4ecd6c9b
saveligulas <saveli.gulas@gmail.com>
2023-01-06 13:04:34 +0100

    Pot Updates

--- src/main/java/SuperClasses/Pot.java
+++ src/main/java/SuperClasses/Pot.java
@@ -5,4 +5,7 @@ import java.util.ArrayList;
 public class Pot {
     private int currentPotSize = 0;
     public ArrayList<Player> playerList = new ArrayList<>();
+
+    public void cleanse() {
+    }
 }
