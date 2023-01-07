341cbc4ebed7018638b03e033677afbcaae94f4a
saveligulas <saveli.gulas@gmail.com>
2023-01-07 22:48:32 +0100

    Made pot public.

--- src/main/java/SuperClasses/Pot.java
+++ src/main/java/SuperClasses/Pot.java
@@ -3,7 +3,7 @@ package SuperClasses;
 import java.util.ArrayList;
 
 public class Pot {
-    private int currentPotSize = 0;
+    public int currentPotSize = 0;
     public ArrayList<Player> playerList = new ArrayList<>();
 
     public void cleanse() {
